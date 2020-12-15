07e9072f0769d2288d2fac2fb619d1e6ed750d27
konovnina <ea.knvn@gmail.com>
2020-12-16 02:33:45 +0400

    fix: tabs switching in main section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
+
+  //Табы
   var tabsItem = $('.recommend__tab');
   var tag = $('.article-heading__tag');
   var title = $('.article-heading__title');
-  var container = $('.main__content')
+  var container = $('.main__content');
+  var author = $('#main-author');
+  var job = $('#job');
+  var avatar = $('#avatar');
 
   tabsItem.on('click', function (event) {
     var tagFrom = $(this).children('.topic-tag');
     var textFrom = $(this).children('.recommend__article-title');
+    var authorFrom = $(this).children('.author');
+    var jobFrom = $(this).children('.job');
+    var avatarFrom = $(this).children('.avatar');
+    var bgFrom = $(this).children('.bg');
 
     var newTag = tagFrom.text();
     var newText = textFrom.text();
     var newColor = tagFrom.css('color');
+    var newAuthor = authorFrom.text();
+    var newJob = jobFrom.text();
+    var newAvatar = avatarFrom.text();
+    var newBG = bgFrom.text();
 
     tabsItem.removeClass('recommend__tab--active');
     $(this).toggleClass('recommend__tab--active')
     title.html(newText);
     tag.html(newTag);
+    author.html(newAuthor);
+    job.html(newJob);
+    avatar.attr('src', newAvatar);
+    container.css('background-image', newBG);
     tag.css('color', newColor);
 
   });
